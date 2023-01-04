baf474ab7444ae558abb245dd2d868ed9b427aee
Ikraanaish <Ikraanaish@icloud.com>
2023-01-04 13:01:22 -0800

    islower

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for i in range(00, 100):
-    print("{:2d}". format(i), end='\n' if i == 99 else ", ")
+    if i < 10:
+        print("0{}". format(i), end=", ")
+    else:
+        print("{}". format(i), end='\n' if i == 99 else ", ")
