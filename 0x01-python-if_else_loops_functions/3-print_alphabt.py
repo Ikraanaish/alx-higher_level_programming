ad7dbe16a7774efb20b7bc6bcf3ccf79b08ac264
Ikraanaish <Ikraanaish@icloud.com>
2023-01-04 12:01:58 -0800

    qand e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for ch in range(97, 123):
     if ch != 101 and ch != 113:
-        print("ASCII value: " + str(ch) + ", Character: ", chr(ch))
+        print("{:c}".format(ch),end='')
 
