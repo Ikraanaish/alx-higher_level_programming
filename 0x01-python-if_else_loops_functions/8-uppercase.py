251671ce77104b9cbb855f2cd360e376ad228195
Ikraanaish <Ikraanaish@icloud.com>
2023-01-05 13:26:38 -0800

    alx

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def uppercase(string):
-    for char in string:
+def uppercase(str):
+    for char in str:
         if ord(char) in range(97, 123):
             is_upper = ord(char)-32
             print("{}".format(chr(is_upper)), end='')
         else:
-            print("{}".format(char), end=' ')
+            print("{}".format(char), end='')
